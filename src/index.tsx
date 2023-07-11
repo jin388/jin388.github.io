5cdc04f78b650f00df23bc53090ce6bdc4493c62
Ruslan Gamzaev <rrrruuus@yandex.ru>
2023-07-11 10:14:12 +0300

    'done'

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./src/App";
+import App from "./App";
 import { Provider } from "react-redux";
-import { setupStore } from "./src/store/store";
+import { setupStore } from "./store/store";
 
 const store = setupStore();
 
